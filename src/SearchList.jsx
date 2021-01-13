b8bdf8a01bbfe893bc5b50721e21953e57f7bafd
Edward Smith <edward.m.smith80@gmail.com>
2021-01-13 08:53:59 -0700

    create SearchList and SearchListItem components

--- src/SearchList.jsx
+++ src/SearchList.jsx
@@ -1,14 +1,62 @@
-import React from 'react';
-import getMovies from './helpers';
+import { React, useState } from 'react';
+import { SearchListItem } from './SearchListItem';
 
-const searchList = "hello";
-// const searchList = getMovies('guard', 1).then(s => s);
+const axios = require('axios');
 
-function SearchList(props) {
+function SearchList() {
+  // create state for search results list
+  const [movieTitles, setMovieTitles] = useState([]);
+  const [movieYears, setMovieYears] = useState([]);
+
+  // get movies from database and set the results to searchResults
+  async function getMovies(searchTerm, pagesToReturn) {
+    // TODO add logic to return more than 1 page of results (IS this the right thing to do?)
+    const maxPages = 1;
+    try {
+      const response = await axios.get(`http://www.omdbapi.com/?s=${searchTerm}&apikey=bbde90f3`);
+      const totalResults = response.data.totalResults;
+      pagesToReturn = (pagesToReturn <= maxPages) ? pagesToReturn : maxPages;
+      pagesToReturn = (pagesToReturn <= totalResults) ? pagesToReturn : totalResults;
+
+      let movies = [];
+      for (let page = 1; page <= pagesToReturn; page++) {
+        const currentMovies = await axios.get(`http://www.omdbapi.com/?s=${searchTerm}&page=${page}&apikey=bbde90f3`);
+        movies = movies.concat(currentMovies.data.Search);
+      }
+      const titles = [];
+      const years = [];
+      for (let movie of movies) {
+        //@TODO trim titles and years for odd characters (some years have a dash in them) use regex to trim to 4 digit years; trim titles for whitespace
+        titles.push(movie.Title);
+        years.push(movie.Year);
+      }
+      setMovieTitles(titles);
+      setMovieYears(years);
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  getMovies('guard', 3);
+
+  // what are we trying return here?
+  const searchListItems = [];
+  for(let i = 0; i < movieTitles.length; i++) {
+    // make an arry of SearchListItems
+    searchListItems.push(
+      <SearchListItem 
+        title={movieTitles[i]}
+        year={movieYears[i]} 
+      />);    
+  }
+
   return (
     <div>
-      <h1>SEARCH LIST</h1>
-      <p>{searchList}</p>
+      <h1>SEARCH RESULTS</h1>
+      <ul>
+        {searchListItems}
+      </ul>
     </div>
   );
 }
--- src/helpers.js
+++ src/helpers.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 // get movie data returns a data object that contains a Search array of objects (up to 10 objects)
 // I have used the 'page' param to return more than 10 results
 
-export default async function getMovies (searchTerm, pagesToReturn) {
+async function getMovies (searchTerm, pagesToReturn) {
   // TODO add logic to return more than 1 page of results (IS this the right thing to do?)
   const maxPages = 1;
   try {
@@ -21,13 +21,15 @@ export default async function getMovies (searchTerm, pagesToReturn) {
       const currentMovies = await axios.get(`http://www.omdbapi.com/?s=${searchTerm}&page=${page}&apikey=bbde90f3`);
       movies = movies.concat(currentMovies.data.Search);
     }
-    return movies;
+    // movies will be a promise ONCE it's returned. right now though, I can use it to set the current search results
+    console.log(movies);
   } catch (error) {
     console.error(error);
   }
 }
 // add movie to search result list
 
+getMovies('guard', 1);
 
 // add movie to nomination list
 
