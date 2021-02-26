f9164bafb34e16600aa3fbfd25af00ae26078c64
Edward Smith <edward.m.smith80@gmail.com>
2021-02-25 23:09:02 -0700

    add type ahead - works but compiles with warnings

--- src/SearchBarResults.jsx
+++ src/SearchBarResults.jsx
@@ -1,16 +1,15 @@
-import { React, useState } from 'react';
+import { React, useEffect, useState } from 'react';
 import axios from 'axios';
 import SearchBarResultItem from './SearchBarResultItem';
 
 
-const SearchBarResults = ({ searchText }) => { // will this component automatically re render when searchText is changed?
+function SearchBarResults({ searchText }) { // will this component automatically re render when searchText is changed?
 
   const [typeAheadResults, setTypeAheadResults] = useState([]);
-  const [searchJustRan, setSearchJustRan] = useState(false);
 
-  // TODO trigger this request and get results before page renders?
 
   async function getTypeAheadResults(s) {
+    console.log('call API - searchText: ', searchText)
     // TODO Hide API KEY
     // the API only returns 10 results at a time
     const response = await axios.get(`https://www.omdbapi.com/?s=${s}*&type=movie&page=1&apikey=bbde90f3`);
@@ -21,23 +20,40 @@ const SearchBarResults = ({ searchText }) => { // will this component automatica
         titles.push(item.Title);
       }
       setTypeAheadResults(titles);
+    } else {
+      setTypeAheadResults([`no results for "${searchText}"`]);
     }
-  }
-  // TODO adjust this test code which only allows additional search if entire text is deleted
-  // search only runs again if the search terms is deleted entirely before something new is
-  // TODO need conditions to check 
-  // 1) when to run first search 
-  // 2) when to rerun search 
-  // 3) variable to track if current searchText returns a result
-  const searchLengths = [3, 6, 9, 12, 15, 18];
-  if (!searchJustRan && searchLengths.indexOf(searchText.length) > -1) {
-    getTypeAheadResults(searchText);
-    setSearchJustRan(true);
-  }
+  };
+  let runNewTypeAhead = false;
+  let typeAheadTimeOut;
 
-  // debounce: Grouping a sudden burst of events (like keystrokes) into a single one.
-  // how would you implment that? how would you group that burst? you track the 
-
+  useEffect(() => {
+    const handleTextChange = (s) => {
+      runNewTypeAhead = true;
+      // debounce: Grouping a sudden burst of events (like keystrokes) into a single one.
+      // how would you implment that? how would you group that burst?
+      // wait 3 seconds, double check that last search term is different from current search term, then run getTypeAheadResults
+      if (typeAheadTimeOut) clearTimeout(typeAheadTimeOut);
+      typeAheadTimeOut = setTimeout(() => {
+        if (runNewTypeAhead) {
+          runNewTypeAhead = false;
+          getTypeAheadResults(s);
+        }
+      }, 200);
+      // TODO adjust this test code which only allows additional search if entire text is deleted
+      // search only runs again if the search terms is deleted entirely before something new is
+      // TODO need conditions to check 
+      // 1) when to run first search 
+      // 2) when to rerun search 
+      // 3) variable to track if current searchText returns a result
+    };
+    // code to run when the prop searchText changes
+    handleTextChange(searchText);
+    return () => {
+      clearTimeout(typeAheadTimeOut);
+    }
+    // this will run getTypeAhead with every single keystroke (even backspaces - how do I change this to only run once every few seconds?)
+  }, [searchText, typeAheadTimeOut]);
 
   const results = typeAheadResults.map((title, index) => {
     return (
